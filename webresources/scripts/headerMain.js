aae2cb9dbe97628ab9a2a84c9421f04b5a2374f7
Globe-City Parks and Resorts <140534873+gcparksandresorts@users.noreply.github.com>
2023-10-04 15:11:53 -0700

    Update headerMain.js

--- webresources/scripts/headerMain.js
+++ webresources/scripts/headerMain.js
@@ -45,7 +45,7 @@
 
 		footBar.innerHTML += "<div class='spacer'></div> \
 				     <div class='container' style='text-align:center;line-height:2;'> \
-				     <b>Get in Touch</b> <br> (312) 988 1000 <br> <a href='mailto: globecityparksandresorts@gmail.com'>guestservices.gcrh@gcpr.net</a> </div>";
+				     <b>Get in Touch</b> <br> (312) 732 1000 <br> <a href='mailto: globecityparksandresorts@gmail.com'>guestservices@phantasmagoria.com</a> </div>";
 		
 		footBar.innerHTML += "<div class='spacer'></div> \
 			              <div style='text-align:center'> <br> \
